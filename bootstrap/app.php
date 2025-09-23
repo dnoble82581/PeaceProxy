857c3bd84717fe0a64508cebdb59240d83289142
Dusty Noble <dnoble82581@gmail.com>
2025-09-23 15:38:11 -0500

    production problems

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,25 +6,25 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-->withRouting(
-    web: __DIR__.'/../routes/web.php',
-    api: __DIR__.'/../routes/api.php',
-    commands: __DIR__.'/../routes/console.php',
-    channels: __DIR__.'/../routes/channels.php',
-    health: '/up',
-)
-->withMiddleware(function (Middleware $middleware) {
-    $middleware->append(App\Http\Middleware\RedirectToTenantDashboardMiddleware::class);
-    $middleware->web(prepend: [
-        SetTenantUrlDefaults::class,
-    ]);
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        channels: __DIR__.'/../routes/channels.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(App\Http\Middleware\RedirectToTenantDashboardMiddleware::class);
+        $middleware->web(prepend: [
+            \App\Http\Middleware\SetTenantUrlDefaultsMiddleware::class,
+        ]);
 
-    // Replace the default guest middleware with our tenant-aware version
-    $middleware->alias([
-        'guest' => RedirectIfAuthenticated::class,
-    ]);
+        // Replace the default guest middleware with our tenant-aware version
+        $middleware->alias([
+            'guest' => RedirectIfAuthenticated::class,
+        ]);
 
-})
-->withExceptions(function (Exceptions $exceptions) {
-    //
-})->create();
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
